d9fb0aceb28a2f7e513f0a8dd319c30d06417444
6qos <42391846+6qos@users.noreply.github.com>
2019-05-08 14:37:38 -0700

    5/8/19

--- notes/Sales Input.py
+++ notes/Sales Input.py
@@ -2,3 +2,10 @@ import csv
 
 with open("Sales Records.csv", "r") as csv_file_thing:
     reader = csv.reader(csv_file_thing)
+    for row in reader:
+        thing = row[13]
+        print(thing)
+        fruit = row[2]
+        print(fruit)
+    if fruit == "Fruit":
+        print(Fruit)
